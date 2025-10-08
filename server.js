af7890eef5199c95609ef4e3edd853adedbf9a90
Jacobo G. Lopez <jaco401@hotmail.com>
2025-10-08 17:57:34 -0500

    updated CORS to include localhost for testing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 // --- ✨ CORS CONFIGURATION ---
 const io = new Server(server, {
   cors: {
-    origin: ["https://loteria.lachinita.com/", "https://host-loteria.lachinita.com/"],
+    origin: ["https://loteria.lachinita.com/", "https://host-loteria.lachinita.com/", "http://localhost:3000"],
     methods: ["GET", "POST"]
   }
 });
